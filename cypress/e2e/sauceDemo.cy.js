95e9476ffc1af7ab3223ef3e7cb22ca58013bf99
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-07-10 12:23:46 +0100

    Test

---
+++ cypress/e2e/sauceDemo.cy.js
@@ -0,0 +1,8 @@
+describe('Login', () => {
+    it('Verify that user can login with valid credentials', () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type("standard_user")
+        cy.get('[data-test="password"]').type("secret_sauce")
+        cy.get('[data-test="login-button"]').click()
+    })
+})
