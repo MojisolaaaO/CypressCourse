0ed598048964fdb21ce27a53efceba9ee47fedb5
MojisolaaaO <mojisolaotusheso@gmail.com>
2024-07-14 19:28:16 +0100

    assertions

--- cypress/e2e/sauceDemo.cy.js
+++ cypress/e2e/sauceDemo.cy.js
@@ -1,8 +1,29 @@
 describe('Login', () => {
-    it('Verify that user can login with valid credentials', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("secret_sauce")
+    before(() => {
+        cy.visit('/')
+
+
+    })
+    it('Verify that user can input their username', () => {
+        cy.get('.login_logo').should("be.visible").wait(2000)
+        cy.get('[data-test="username"]').type("standard_user").wait(2000)
+        cy.get('[data-test="username"]').should("have.value", "standard_user")
+
+    })
+
+    it('Verify that user can input their password', () => {
+        cy.get('[data-test="password"]').type("secret_sauce").wait(2000)
         cy.get('[data-test="login-button"]').click()
+        cy.screenshot('Test')
+
+    })
+
+    it('Verify that user cannot login with invalid credentials', () => {
+        cy.visit('/')
+        cy.get('[data-test="username"]').type("standard_use")
+        cy.get('[data-test="password"]').type("secret_sauce").wait(2000)
+        cy.get('[data-test="login-button"]').click()
+        cy.get('[data-test="error"]').should("be.visible")
+
     })
 })
